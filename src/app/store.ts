7b877972e25039561f9b3822f38068bd556b528f
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-10-09 23:22:33 +0300

    refactor store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,24 +1,22 @@
-import { AnyAction, combineReducers } from "redux"
 import { tasksReducer } from "features/TodolistsList/tasksReducer"
 import { todolistsReducer } from "features/TodolistsList/todolistsReducer"
-import { ThunkDispatch } from "redux-thunk"
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
 import { appReducer } from "app/appReducer"
 import { authReducer } from "features/Login/authReducer"
 import { configureStore } from "@reduxjs/toolkit"
 
-const rootReducer = combineReducers({
-  tasks: tasksReducer,
-  todolists: todolistsReducer,
-  app: appReducer,
-  auth: authReducer,
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    todolists: todolistsReducer,
+    app: appReducer,
+    auth: authReducer,
+  },
 })
-
-export const store = configureStore({ reducer: rootReducer })
 
 export type AppRootState = ReturnType<typeof store.getState>
 
-export type AppDispatch = ThunkDispatch<AppRootState, any, AnyAction>
+export type AppDispatch = typeof store.dispatch
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 
 export const useAppSelector: TypedUseSelectorHook<AppRootState> = useSelector
